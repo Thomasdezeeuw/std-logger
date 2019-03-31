49c790d3e0d975ec5047f2ae0bcbe6729931963f
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2019-03-31 14:39:58 +0200

    Rewrite benchmark to use Criterion

--- benches/time_format.rs
+++ benches/time_format.rs
@@ -1,12 +1,6 @@
-#![feature(test)]
+use criterion::{Criterion, criterion_group, criterion_main};
 
-extern crate chrono;
-extern crate test;
-
-use test::Bencher;
-
-#[bench]
-fn time_formatting_with_chrono_format(b: &mut Bencher) {
+fn chrono_format(c: &mut Criterion) {
     use chrono::format::Fixed::Nanosecond6;
     use chrono::format::Item::{self, Fixed, Literal, Numeric};
     use chrono::format::Numeric::{Day, Hour, Minute, Month, Second, Year};
@@ -28,22 +22,29 @@ fn time_formatting_with_chrono_format(b: &mut Bencher) {
         // We're always printing a UTC timezone, no need to print the offset.
     ];
 
-    let timestamp = chrono::Utc::now();
-    b.iter(|| {
-        format!("{} [{}] {}: {}\n",
-            timestamp.format_with_items(FORMAT_ITEMS.iter().cloned()),
-            "INFO", "target", "Some message");
-    })
+    c.bench_function("chrono format", |b| {
+        let timestamp = chrono::Utc::now();
+        b.iter(|| {
+            format!("{} [{}] {}: {}\n",
+                timestamp.format_with_items(FORMAT_ITEMS.iter().cloned()),
+                "INFO", "target", "Some message");
+        })
+    });
 }
 
-#[bench]
-fn time_formatting_with_manual_format(b: &mut Bencher) {
+fn manual_format(c: &mut Criterion) {
     use chrono::{Datelike, Timelike};
-    let timestamp = chrono::Utc::now();
-    b.iter(|| {
-        format!("{:004}-{:02}-{:02}T{:02}:{:02}:{:02}.{:06}Z [{}] {}: {}\n",
+
+    c.bench_function("manual format", |b| {
+        let timestamp = chrono::Utc::now();
+        b.iter(|| {
+            format!("{:004}-{:02}-{:02}T{:02}:{:02}:{:02}.{:06}Z [{}] {}: {}\n",
             timestamp.year(), timestamp.month(), timestamp.day(),
             timestamp.hour(), timestamp.minute(), timestamp.second(),
             timestamp.nanosecond(), "INFO", "target", "Some message");
-    })
+        })
+    });
 }
+
+criterion_group!(time_format, chrono_format, manual_format);
+criterion_main!(time_format);
