d7c9e342c2f50209f71370903e1b0006b24afa55
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2019-11-22 17:17:12 +0100

    Add printing of key-value pairs

---
+++ examples/key_value.rs
@@ -0,0 +1,81 @@
+use log;
+use std_logger::REQUEST_TARGET;
+
+fn main() {
+    // Initialize the logger.
+    std_logger::init();
+
+    // Fake the handling of a request.
+    logger_middleware(Request {
+        url: "/".to_owned(),
+        method: "GET".to_owned(),
+    });
+}
+
+// Our fake HTTP request.
+struct Request {
+    url: String,
+    method: String,
+}
+
+// Our fake HTTP response.
+struct Response {
+    status_code: u16,
+    body: String,
+}
+
+fn logger_middleware(request: Request) -> Response {
+    // Clone the url and method. Note: don't actually do this in an HTTP this is
+    // rather wastefull to.
+    let url = request.url.clone();
+    let method = request.url.clone();
+
+    // Call our handler.
+    let response = http_handler(request);
+
+    log::info!("Hello world");
+
+    let kvs: Vec<Box<dyn log::kv::Source>> = vec![
+            Box::new(("url", &url)),
+            Box::new(("method", &method)),
+            Box::new(("status_code", response.status_code)),
+            Box::new(("body_size", response.body.len() as u64)),
+    ];
+
+    let record = log::Record::builder()
+        .args(format_args!("got request"))
+        .level(log::Level::Info)
+        .target(REQUEST_TARGET)
+        .file(Some(file!()))
+        .line(Some(line!()))
+        .module_path(Some(module_path!()))
+        .key_values(&kvs)
+        .build();
+    log::logger().log(&record);
+
+    let record = log::Record::builder()
+        .args(format_args!("some message"))
+        .level(log::Level::Info)
+        .target("some_target")
+        .file(Some(file!()))
+        .line(Some(line!()))
+        .module_path(Some(module_path!()))
+        .key_values(&("single", "value"))
+        .build();
+    log::logger().log(&record);
+
+    response
+}
+
+fn http_handler(request: Request) -> Response {
+    match (request.method.as_str(), request.url.as_str()) {
+        ("GET", "/") => Response {
+            status_code: 200,
+            body: "Home page".to_owned(),
+        },
+        _ => Response {
+            status_code: 404,
+            body: "Not found".to_owned(),
+        },
+    }
+}
