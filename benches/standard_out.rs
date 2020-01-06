84d0f1094e3e958398f8e5344ac0e6c2a4a68e42
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2020-01-06 13:12:02 +0100

    Add benchmark of use io::stdout vs libc directly

---
+++ benches/standard_out.rs
@@ -0,0 +1,34 @@
+use std::io::{self, stdout, Write};
+
+use criterion::{criterion_group, criterion_main, Criterion};
+
+const MSG: &[u8] = b"2020-01-06T12:00:09.514249Z [REQUEST] simple: url = `/not_found`, method = `/not_found`, status_code = 404, body_size = 9";
+
+fn via_std_lib(c: &mut Criterion) {
+    c.bench_function("via_std_lib", |b| {
+        b.iter(|| {
+            stdout().write(MSG).expect("write error");
+        })
+    });
+}
+
+fn via_libc_fd(c: &mut Criterion) {
+    c.bench_function("via_libc_fd", |b| {
+        b.iter(|| {
+            match unsafe {
+                libc::write(
+                    libc::STDOUT_FILENO,
+                    MSG.as_ptr() as *const libc::c_void,
+                    MSG.len(),
+                )
+            } {
+                n if n < 0 => Err(io::Error::last_os_error()),
+                n => Ok(n as usize),
+            }
+            .expect("write error");
+        })
+    });
+}
+
+criterion_group!(standard_out, via_std_lib, via_libc_fd);
+criterion_main!(standard_out);
