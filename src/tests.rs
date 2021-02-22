3c1016cd0aa1d6f4c9ebf14fd61ba78427468e74
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2021-02-22 13:25:40 +0100

    Change to logfmt format
    The logfmt doesn't really have a standard, but it is described here:
    https://www.brandur.org/logfmt, along with its best practices.
    
    The new implementation uses vectored writing, reducing the amount of
    copying when formatting the log record.

--- src/tests.rs
+++ src/tests.rs
@@ -1,20 +1,21 @@
-use std::default::Default;
+use std::io::{IoSlice, Write};
+use std::mem::replace;
+use std::ops::Deref;
 use std::sync::Mutex;
 #[cfg(feature = "timestamp")]
 use std::time::{Duration, SystemTime};
-use std::{env, panic, str};
+use std::{env, fmt, panic, str};
 
 use lazy_static::lazy_static;
-use log::{debug, error, info, trace, warn, LevelFilter};
+use log::{debug, error, info, kv, trace, warn, Level, LevelFilter, Record};
 
 use crate::{
-    get_log_targets, get_max_level, init, request, Targets, LOG_OUTPUT, LOG_OUTPUT_INDEX,
-    REQUEST_TARGET,
+    format, get_log_targets, get_max_level, init, request, Targets, LOG_OUTPUT, REQUEST_TARGET,
 };
 
 /// Macro to create a group of sequential tests.
 macro_rules! sequential_tests {
-    ($(fn $name:ident() $body:block)+) => {
+    ( $(fn $name: ident () $body: block)+ ) => {
         lazy_static! {
             /// A global lock for testing sequentially.
             static ref SEQUENTIAL_TEST_MUTEX: Mutex<()> = Mutex::new(());
@@ -36,7 +37,7 @@ macro_rules! sequential_tests {
 
 sequential_tests! {
     fn should_get_the_correct_log_level_from_env() {
-        let tests = vec![
+        let tests = &[
             ("LOG", "TRACE", LevelFilter::Trace),
             ("LOG", "ERROR", LevelFilter::Error),
             ("LOG_LEVEL", "ERROR", LevelFilter::Error),
@@ -45,31 +46,36 @@ sequential_tests! {
             ("DEBUG", "1", LevelFilter::Debug),
         ];
 
-        for test in tests {
-            env::set_var(test.0, test.1);
+        for (env_var, env_val, want) in tests {
+            env::set_var(env_var, env_val);
 
-            let want = test.2;
             let got = get_max_level();
-            assert_eq!(want, got);
+            assert_eq!(*want, got);
 
-            env::remove_var(test.0);
+            env::remove_var(env_var);
         }
+
+        // Should default to info.
+        env::remove_var("TRACE");
+        env::remove_var("DEBUG");
+        env::remove_var("LOG");
+        env::remove_var("LOG_LEVEL");
+        assert_eq!(get_max_level(), LevelFilter::Info);
     }
 
     fn should_get_correct_log_targets() {
-        let tests = vec![
+        let tests = &[
             ("", Targets::All),
             ("crate1", Targets::Only(vec!["crate1".into()].into_boxed_slice())),
             ("crate1::mod1", Targets::Only(vec!["crate1::mod1".into()].into_boxed_slice())),
             ("crate1,crate2", Targets::Only(vec!["crate1".into(), "crate2".into()].into_boxed_slice())),
         ];
 
-        for test in tests {
-            env::set_var("LOG_TARGET", test.0);
+        for (env_val, want) in tests {
+            env::set_var("LOG_TARGET", env_val);
 
-            let want = test.1;
             let got = get_log_targets();
-            assert_eq!(want, got);
+            assert_eq!(*want, got);
         }
 
         env::remove_var("LOG_TARGET");
@@ -77,7 +83,21 @@ sequential_tests! {
     }
 
     fn log_output() {
-        unsafe { log_setup(); }
+        LOG_OUTPUT.lock().unwrap().clear();
+
+        env::set_var("LOG_LEVEL", "TRACE");
+        init();
+        env::remove_var("LOG_LEVEL");
+
+        let want = &[
+            "lvl=\"TRACE\" msg=\"trace message\" target=\"std_logger::tests\" module=\"std_logger::tests\" file=\"src/tests.rs:105\"\n",
+            "lvl=\"DEBUG\" msg=\"debug message\" target=\"std_logger::tests\" module=\"std_logger::tests\" file=\"src/tests.rs:106\"\n",
+            "lvl=\"INFO\" msg=\"info message\" target=\"std_logger::tests\" module=\"std_logger::tests\" file=\"src/tests.rs:107\"\n",
+            "lvl=\"WARN\" msg=\"warn message\" target=\"std_logger::tests\" module=\"std_logger::tests\" file=\"src/tests.rs:108\"\n",
+            "lvl=\"ERROR\" msg=\"error message\" target=\"std_logger::tests\" module=\"std_logger::tests\" file=\"src/tests.rs:109\"\n",
+            "lvl=\"INFO\" msg=\"request message1\" target=\"request\" module=\"std_logger::tests\" file=\"src/tests.rs:110\"\n",
+            "lvl=\"INFO\" msg=\"request message2\" target=\"request\" module=\"std_logger::tests\" file=\"src/tests.rs:111\"\n",
+        ];
 
         #[cfg(feature = "timestamp")]
         let timestamp = SystemTime::now();
@@ -90,76 +110,32 @@ sequential_tests! {
         info!(target: REQUEST_TARGET, "request message1");
         request!("request message2");
 
-        let want = vec![
-            "[TRACE] std_logger::tests (src/tests.rs:85): trace message",
-            "[DEBUG] std_logger::tests (src/tests.rs:86): debug message",
-            "[INFO] std_logger::tests (src/tests.rs:87): info message",
-            "[WARN] std_logger::tests (src/tests.rs:88): warn message",
-            "[ERROR] std_logger::tests (src/tests.rs:89): error message",
-            "[REQUEST] std_logger::tests (src/tests.rs:90): request message1",
-            "[REQUEST] std_logger::tests (src/tests.rs:91): request message2",
-        ];
-        let mut got = unsafe {
-            (&*LOG_OUTPUT).iter()
-        };
+        let got = replace(&mut *(LOG_OUTPUT.lock().unwrap()), Vec::new());
+        // Make sure the panics aren't logged.
+        let _ = std::panic::take_hook();
 
         let mut got_length = 0;
-        let mut want_iter = want.iter();
-        loop {
-            match (want_iter.next(), got.next()) {
-                (Some(want), Some(got)) if got.is_some() => {
-                    let got = got.as_ref().unwrap();
-                    let got = str::from_utf8(got).expect("unable to parse string").trim();
+        for (want, got) in want.into_iter().zip(got.into_iter()) {
+            let got = str::from_utf8(&got).expect("unable to parse string");
 
-                    #[allow(unused_mut)]
-                    let mut want = (*want).to_owned();
-                    #[cfg(feature = "timestamp")]
-                    { want = add_timestamp(want, timestamp, got); }
+            #[allow(unused_mut)]
+            let mut want = (*want).to_owned();
+            #[cfg(feature = "timestamp")]
+            { want = add_timestamp(want, timestamp, got); }
 
-                    // TODO: for some reason this failure doesn't shows itself in the
-                    // output, hence this workaround.
-                    println!("Comparing:");
-                    println!("want: {}", want);
-                    println!("got:  {}", got);
-                    assert_eq!(got, want.as_str(), "message differ");
-
-                    got_length += 1;
-                },
-                _ => break,
-            }
+            assert_eq!(got, want.as_str(), "message differ");
+            got_length += 1;
         }
 
-        if got_length != want.len() {
-            panic!("the number of log messages got differs from the amount of messages wanted");
-        }
+        assert_eq!(got_length, want.len(), "the number of log messages got differs from the amount of messages wanted");
     }
-}
-
-/// This requires the `SEQUENTIAL_TEST_MUTEX` to be held!
-unsafe fn log_setup() {
-    use std::sync::atomic::Ordering;
-
-    if !LOG_OUTPUT.is_null() {
-        for output in (&mut *LOG_OUTPUT).iter_mut().skip(1) {
-            drop(output.take());
-        }
-        LOG_OUTPUT_INDEX.store(1, Ordering::Relaxed);
-        return;
-    }
-
-    let output = Box::new(Default::default());
-    LOG_OUTPUT = Box::into_raw(output);
-
-    env::set_var("LOG_LEVEL", "TRACE");
-    init();
-    env::remove_var("LOG_LEVEL");
 }
 
 #[cfg(feature = "timestamp")]
-fn add_timestamp(message: String, now: SystemTime, got: &str) -> String {
+fn add_timestamp(message: String, timestamp: SystemTime, got: &str) -> String {
     use std::mem::MaybeUninit;
 
-    let diff = now
+    let diff = timestamp
         .duration_since(SystemTime::UNIX_EPOCH)
         .unwrap_or(Duration::new(0, 0));
     let mut tm = MaybeUninit::uninit();
@@ -179,21 +155,21 @@ fn add_timestamp(message: String, now: SystemTime, got: &str) -> String {
 
     // Add the timestamp to the expected string.
     let timestamp = format!(
-        "{:004}-{:02}-{:02}T{:02}:{:02}:{:02}.{}Z",
+        "ts=\"{:004}-{:02}-{:02}T{:02}:{:02}:{:02}.{}Z\"",
         year,
         month,
         day,
         hour,
         min,
         sec,
-        &got[20..26] // We can never match the microseconds, so we just copy them.
+        &got[24..30] // We can never match the microseconds, so we just copy them.
     );
     format!("{} {}", timestamp, message)
 }
 
 #[test]
 fn targets_should_log() {
-    let targets = vec![
+    let targets = &[
         Targets::All,
         Targets::Only(vec!["crate1".into()].into_boxed_slice()),
         Targets::Only(vec!["crate1::mod1".into()].into_boxed_slice()),
@@ -219,4 +195,73 @@ fn targets_should_log() {
             )
         }
     }
+}
+
+#[test]
+fn format() {
+    struct MyDisplay;
+
+    impl fmt::Display for MyDisplay {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "MyDisplay")
+        }
+    }
+
+    let record1 = Record::builder()
+        .args(format_args!("some arguments1"))
+        .level(Level::Info)
+        .target("some_target1")
+        .module_path_static(Some("module_path1"))
+        .file_static(Some("file1"))
+        .line(Some(123))
+        .key_values(&("key1", "value1"))
+        .build();
+    let kvs = &[
+        ("key2a", (&"value2") as &dyn kv::ToValue),
+        ("key2b", &123u64),
+        ("key3c", &-123i64),
+        ("key3d", &123.0f64),
+        ("key2e", &true),
+        ("key2f", &false),
+        ("key2g", &'c'),
+        ("key2g", &(&MyDisplay as &dyn fmt::Display)),
+    ];
+    let kvs: &[(&str, &dyn kv::ToValue)] = kvs.deref();
+    let kvs: &dyn kv::Source = &kvs;
+    let record2 = Record::builder()
+        .args(format_args!("arguments2"))
+        .level(Level::Error)
+        .target("second_target")
+        .module_path_static(Some("module_path1"))
+        .file_static(Some("file2"))
+        .line(Some(111))
+        .key_values(kvs)
+        .build();
+
+    let tests = &[
+        (record1.clone(), true, "lvl=\"INFO\" msg=\"some arguments1\" target=\"some_target1\" module=\"module_path1\" key1=\"value1\" file=\"file1:123\"\n"),
+        (record1, false, "lvl=\"INFO\" msg=\"some arguments1\" target=\"some_target1\" module=\"module_path1\" key1=\"value1\"\n"),
+        (record2, true, "lvl=\"ERROR\" msg=\"arguments2\" target=\"second_target\" module=\"module_path1\" key2a=\"value2\" key2b=123 key3c=-123 key3d=123.0 key2e=true key2f=false key2g=\"c\" key2g=\"MyDisplay\" file=\"file2:111\"\n"),
+    ];
+
+    for (record, debug, want) in tests {
+        let got = format_record(record, *debug);
+        #[allow(unused_mut)]
+        let mut want = (*want).to_owned();
+        #[cfg(feature = "timestamp")]
+        {
+            want = add_timestamp(want, SystemTime::now(), &got)
+        }
+
+        assert_eq!(got, *want);
+    }
+}
+
+fn format_record(record: &Record, debug: bool) -> String {
+    let mut bufs = [IoSlice::new(&[]); crate::BUFS_SIZE];
+    let mut buf = format::Buffer::new();
+    let bufs = format::record(&mut bufs, &mut buf, record, debug);
+    let mut output = Vec::new();
+    let _ = output.write_vectored(bufs).unwrap();
+    String::from_utf8(output).unwrap()
 }
