f8aff06ea4345af230083149fd7d1717e1391bb3
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2021-04-29 20:39:00 +0200

    Correctly handle panics during logging
    Previously when a panic occurred during the formatting/logging of a
    record, and with the log-panic feature enabled, it would log again. This
    meant that the log function would be called again, but this time the BUF
    thread-local was in use. Trying to borrow this thread-local again would
    panic as shown in issue 42. The following code was enough to trigger the
    bug.
    
        struct T;
    
        impl fmt::Display for T {
            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
                panic!()
            }
        }
    
        info!("T: {}", T);
    
    This commit fixes the problem by allocating a new buffer in the rare
    case this happens.

---
+++ tests/regression_42.rs
@@ -0,0 +1,24 @@
+//! Regression test for issue #42.
+
+#![cfg(feature = "log-panic")]
+
+use std::fmt;
+
+use log::info;
+
+/// Panicking while logging triggers another `log!`.
+#[test]
+#[should_panic = "panic during formatting"]
+fn panicking_while_logging() {
+    std_logger::init();
+
+    struct T;
+
+    impl fmt::Display for T {
+        fn fmt(&self, _f: &mut fmt::Formatter) -> fmt::Result {
+            panic!("panic during formatting")
+        }
+    }
+
+    info!("{}", T);
+}
